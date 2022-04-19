15432679c532984d952ac3172fc82146be15278d
faheelsattar <faheelsattar@yahoo.com>
2022-04-19 15:26:21 +0500

    refactor: deposit custom removed

--- src/XVaults.sol
+++ src/XVaults.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.10;
 
-//]Mixins
+//Mixins
 import {ERC4626} from "solmate/mixins/ERC4626.sol";
 
 //Tokens
@@ -16,13 +16,5 @@ import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 
 contract XVaults is ERC4626 {
-    function deposit(uint256 assets, address receiver)
-        public
-        override
-        returns (uint256 shares)
-    {
-        // usage: user will deposit a single asset and get relevent lp tokens.
-        // sanitize inputs to check for user balance of the asset being deposited
-        // calculate shares according to a formula
-    }
+    
 }
